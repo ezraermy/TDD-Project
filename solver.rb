3db89e442af0bf513052cbe2b4a577dae226ba6f
Ezra <ezraermy@gmail.com>
2023-06-01 15:08:14 +0300

    Fix linters

--- solver.rb
+++ solver.rb
@@ -1,27 +1,30 @@
 class Solver
-    def factorial(n)
-        raise ArgumentError, "Argument must be non-negative" if n.negative?
-        return 1 if n.zero?
-        result = 1
-        (1..n).each do |i|
-            result *= i
-        end
-        result
-    end
+  def factorial(num)
+    raise ArgumentError, 'Argument must be non-negative' if num.negative?
+    return 1 if num.zero?
 
-    def reverse(str)
-        result = ""
-        return result if str.empty?
-        (0..str.length - 1).each do |i|
-            result += str[str.length - 1 - i]
-        end
-        result
+    result = 1
+    (1..num).each do |i|
+      result *= i
     end
+    result
+  end
 
-    def fizzbuzz(n)
-        return "FizzBuzz" if (n % 3).zero? && (n % 5).zero?
-        return "Fizz" if (n % 3).zero?
-        return "Buzz" if (n % 5).zero?
-        n.to_s
+  def reverse(str)
+    result = ''
+    return result if str.empty?
+
+    (0..str.length - 1).each do |i|
+      result += str[str.length - 1 - i]
     end
+    result
+  end
+
+  def fizzbuzz(num)
+    return 'FizzBuzz' if (num % 3).zero? && (num % 5).zero?
+    return 'Fizz' if (num % 3).zero?
+    return 'Buzz' if (num % 5).zero?
+
+    num.to_s
+  end
 end
